18acff15c85cf22a2c4dbd26b8017adeefc09b2b
Alfred Ochieng <techyobura@gmail.com>
2024-05-14 12:52:48 +0300

    wueh 2

--- api/index.js
+++ api/index.js
@@ -26,11 +26,8 @@ app.use(cors(corsOptions)); */
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", [
-    "Content-Type",
-    "authorization",
-  ]);
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
