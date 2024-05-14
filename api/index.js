ce9cc9cffe1f5449addaae98b02ac92857054584
Alfred Ochieng <techyobura@gmail.com>
2024-05-14 12:47:43 +0300

    wueeh cors

--- api/index.js
+++ api/index.js
@@ -21,14 +21,11 @@ connectDB();
 //middleware
 app.use(express.json());
 
-app.use(credentials);
-app.use(cors(corsOptions));
+/* app.use(credentials);
+app.use(cors(corsOptions)); */
 
 app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://alfredochieng.netlify.app"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", [
     "Content-Type",
