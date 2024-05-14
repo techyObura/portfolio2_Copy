7d6ea4a3a1c9f2149cb6638b23d9e76bc25026f1
Alfred Ochieng <techyobura@gmail.com>
2024-05-15 01:00:39 +0300

    tgfx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -67,7 +67,7 @@ const Register = ({ menu }) => {
       <div className="flex md:flex-row px-2 gap-5 md:items-center mx-auto flex-col bg-[url('./alfred1.png')]  bg-no-repeat bg-contain bg-right-top sm:bg-none">
         {errorMessage && (
           <Alert
-            className="relative top-8 text-center self-center h-8 w-[97%] mt-1"
+            className=" text-center self-center h-8 w-[97%] mt-1"
             color={"failure"}
           >
             {errorMessage}
