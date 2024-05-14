457e6d57187d500bdca88c047fe40172b5fbf652
Alfred Ochieng <techyobura@gmail.com>
2024-05-14 22:33:34 +0300

    otis

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -30,14 +30,14 @@ const Projects = ({ menu }) => {
   return (
     <div
       className={`relative min-h-screen ${
-        menu ? "top-[45vw] md:top-32 lg:top-32" : "top-32 md:top-32 lg:top-32"
+        menu ? "top-[48vw] md:top-32 lg:top-32" : "top-32 md:top-32 lg:top-32"
       } `}
     >
       <div className=" px-3  flex flex-col items-center justify-center">
         {path === "/projects" && (
           <div
             className={` fixed py-1   w-[100vw] md:w-[90vw] rounded-md shadow-md h-[1px] pb-1 bg-gray-200 dark:bg-teal-500 z-10 ${
-              menu ? "top-[42vw] md:top-[70px]" : "top-[60px] md:top-[70px]"
+              menu ? "top-[45vw] md:top-[70px]" : "top-[60px] md:top-[70px]"
             }`}
           >
             <h4 className="text-2xl sm:text-3xl pb-2 bg-gray-200 shadow-md dark:bg-teal-500 mt-0  text-center  ">
