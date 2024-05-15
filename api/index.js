9a2b1630ce972f6a078ae35954458b4af2630bdd
Alfred Ochieng <techyobura@gmail.com>
2024-05-15 15:20:47 +0300

    rwggs

--- api/index.js
+++ api/index.js
@@ -27,7 +27,11 @@ app.use(cors(corsOptions)); */
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Headers", [
+    "Content-Type",
+    "Authorization",
+    "authorization",
+  ]);
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
